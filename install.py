b5532fc9178aa25358f865d707ea739023686e74
BrinkerVII <brinkervii@gmail.com>
2019-04-10 15:42:23 +0200

    Add python 3.7 install guard

--- install.py
+++ install.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
-if __name__ == "__main__":
+def perform_install():
     import os
-
     os.execlp("bash", "bash", "./install.sh")
+
+
+def err_py37():
+    import tkinter
+    from tkinter import messagebox
+
+    root = tkinter.Tk()
+    root.withdraw()
+
+    messagebox.showerror("Out of date",
+                         "Your current version of python is out of date and therefore Grapejuice cannot be installer")
+
+
+def have_py37():
+    import sys
+    satisfied = sys.version_info >= (3, 7)
+    if not satisfied:
+        err_py37()
+
+    return satisfied
+
+
+if __name__ == "__main__":
+    if have_py37():
+        perform_install()
