57fb14502912966e05f23a16c46bec7e82e977df
BrinkerVII <brinkervii@gmail.com>
2019-04-10 13:07:38 +0200

    Update build

--- pkg/configure-debian-deps.sh
+++ pkg/configure-debian-deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt update
-apt install build-essential libcairo2-dev libgirepository1.0-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev zlib1g-dev zlibc libgtk-3-0 gir1.2-gtk-3.0 libgdk-pixbuf2.0-dev libpangox-1.0-dev libgtk-3-0 libgtk-3-dev gnome-shell -y
+apt install build-essential appstream libcairo2-dev libgirepository1.0-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev zlib1g-dev zlibc libgtk-3-0 gir1.2-gtk-3.0 libgdk-pixbuf2.0-dev libpangox-1.0-dev libgtk-3-0 libgtk-3-dev gnome-shell -y
 
 systemctl disable display-manager
 systemctl disable gdm3
