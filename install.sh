ae7f2788cb0ee49fee16f7ff9fb3d80219bd9705
BrinkerVII <brinkervii@gmail.com>
2019-04-10 13:27:36 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -2,13 +2,15 @@
 
 OLD_CWD=`pwd`
 
+PYTHON=`which python3`
+
 APPLICATION_DIR=$HOME/.local/share/grapejuice
 mkdir -p ${APPLICATION_DIR}
 cp -frax . ${APPLICATION_DIR}
 export PYTHONPATH=${APPLICATION_DIR}/src
 
 cd ${APPLICATION_DIR}
-virtualenv -p `which python3` venv
+${PYTHON} -m venv venv
 source ./venv/bin/activate
 pip install -r requirements.txt
 
