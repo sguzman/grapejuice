2f12d900a588821e0dea657602951553604eff16
BrinkerVII <brinkervii@gmail.com>
2019-04-10 14:26:44 +0200

    Remove old scripts

--- src/grapejuice/install-packages.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-virtualenv -p `which python3` venv
-source ./venv/bin/activate
-pip install -r requirements.txt
-
-deactivate
--- src/grapejuice/set-bits.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-chmod +x bin/grapejuice
-chmod +x uninstall.py
-chmod +x grapejuice.py
