e5a0636b2b7d5b70fd3d850a0f501ac097ea142b
BrinkerVII <brinkervii@gmail.com>
2019-04-21 13:19:55 +0200

    Remove old window_base

--- src/grapejuice/_internal/window_base.py
+++
@@ -1,8 +0,0 @@
-class WindowBase:
-    def __init__(self, glade_path, handler_class=None):
-        from gi.repository import Gtk
-
-        self.builder = Gtk.Builder()
-        self.builder.add_from_file(glade_path)
-        if handler_class:
-            self.builder.connect_signals(handler_class())
