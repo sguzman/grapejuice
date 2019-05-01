16c22c74e534714d9e5df172727e8a5092e52a74
BrinkerVII <brinkervii@gmail.com>
2019-05-02 00:39:13 +0200

    Change main window title

--- src/grapejuice/__main__.py
+++ src/grapejuice/__main__.py
@@ -3,4 +3,5 @@ import sys
 from grapejuice._internal import main as main
 
 if __name__ == "__main__":
+    sys.argv[0] = "Grapejuice"  # Hope this doesn't break anything
     sys.exit(main(sys.argv))
