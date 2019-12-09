d1c4223f3e814754520bdd1120469ec5b7302700
BrinkerVII <brinkervii@gmail.com>
2019-12-09 20:46:44 +0100

    Add more checks to the installation script

--- install.sh
+++ install.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
 
+try_deactivate () {
+    if command -v deactivate; then
+        deactivate
+    fi
+}
+
+is_command_present() {
+    if command -v "$1"; then
+        echo "The command $1 is present"
+    else
+        echo "You are missing the command $1, please follow the Grapejuice installation guide and install all dependencies"
+        try_deactivate
+        exit 1
+    fi
+}
+
+virtualenv_failed() {
+    echo "Failed to configure virtualenv"
+    try_deactivate
+    exit 1
+}
+
+pip_failed() {
+    echo "Failed to install python dependencies"
+    try_deactivate
+    exit 1
+}
+
+is_command_present virtualenv
+
 OLD_CWD=$(pwd)
 
 PYTHON=/usr/bin/python3.7
@@ -23,12 +53,17 @@ rm -rf ./build
 
 chmod +x "$APPLICATION_DIR/bin/grapejuice"
 
-virtualenv -p "$PYTHON" venv
+virtualenv -p "$PYTHON" venv || virtualenv_failed
 source ./venv/bin/activate
-pip install -r requirements.txt
+is_command_present pip
+pip install -r requirements.txt || pip_failed
 
 deactivate
 
 ./bin/grapejuice post_install
 
 cd "$OLD_CWD" || exit 1
+
+echo
+echo "Grapejuice is now installed, it should be available in your application launcher or menu."
+echo "If Grapejuice does not appear in your menu or launcher, you might have to log out and back in again."
