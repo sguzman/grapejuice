f48d4b5b500e59163528cdb30540ffde37e66683
BrinkerVII <brinkervii@gmail.com>
2020-01-10 16:25:00 +0100

    Include grapejuice src path in sys.path

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 import os
+import sys
+
+here = os.path.abspath(os.path.dirname(__file__))
+project_path = here
+src_path = os.path.join(project_path, "src")
+readme_path = os.path.join(project_path, "README.md")
+requirements_path = os.path.join(project_path, "requirements.txt")
+
+sys.path.insert(0, src_path)
 
 from setuptools import setup, find_packages
 
 from grapejuice.__init__ import __version__ as grapejuice_version
-
-here = os.path.abspath(os.path.dirname(__file__))
-project_path = here
-readme_path = os.path.join(project_path, "README.md")
-requirements_path = os.path.join(project_path, "requirements.txt")
 
 with open(readme_path, "r") as fp:
     long_description = fp.read()
