47da9773bffcd7d71ea5a80b61bc80e9d5fca713
BrinkerVII <brinkervii@gmail.com>
2020-01-29 13:56:47 +0100

    Add gui settings backbone

--- src/grapejuice_common/settings.py
+++ src/grapejuice_common/settings.py
@@ -2,8 +2,45 @@ import atexit
 import json
 import os
 import time
+from typing import Union
 
 from grapejuice_common import variables
+
+
+class Setting:
+    def __init__(self, key: str, display_name: str, default_value: Union[any, None] = None):
+        self._key: str = key
+        self._display_name: str = display_name
+        self._default_value: Union[any, None] = default_value
+        self._value: any = default_value
+
+    def isinstance(self, v):
+        return isinstance(self.value, v)
+
+    def reset(self):
+        self._value = self._default_value
+
+    @property
+    def key(self) -> str:
+        return self._key
+
+    @property
+    def value(self) -> Union[any, None]:
+        return self._value
+
+    @value.setter
+    def value(self, v) -> None:
+        if self._default_value is not None:
+            assert type(self._default_value) is type(v)
+
+        self._value = v
+
+    @property
+    def display_name(self) -> str:
+        return self._display_name
+
+    def __repr__(self) -> str:
+        return f"'{self.key}' -> '{self.value}' ({self._default_value})"
 
 
 class UserSettings:
@@ -14,6 +51,8 @@ class UserSettings:
         self.n_player_dialogs_remain = 3
         self.show_fast_flag_warning = True
 
+        self.wine_binary = Setting("wine_binary", "Wine binary", "")
+
         self._update_last_run()
 
         self.load()
@@ -22,7 +61,10 @@ class UserSettings:
         d = dict()
 
         for k, v in self.__dict__.items():
-            if not k.startswith("_"):
+            if isinstance(v, Setting):
+                d[v.key] = v.value
+
+            elif not k.startswith("_"):
                 d[k] = v
 
         return d
@@ -34,6 +76,12 @@ class UserSettings:
         self_dict = self.__dict__
 
         for k, v in o.items():
+            if k in self_dict:
+                existing_value = self_dict[k]
+                if isinstance(existing_value, Setting):
+                    existing_value.value = v
+                    continue
+
             self_dict[k] = v
 
     def load(self):
@@ -50,8 +98,12 @@ class UserSettings:
         with open(self._location, "w+") as fp:
             json.dump(self._filtered_dict(), fp)
 
+    def ui_facing_settings(self):
+        return list(filter(lambda v: isinstance(v, Setting), self.__dict__.values()))
+
 
 settings = UserSettings()
+print(settings.ui_facing_settings())
 
 
 def save_settings():
