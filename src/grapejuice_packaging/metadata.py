1fe4c245180e94bc585a340d629cd2c629b1f8b5
BrinkerVII <brinkervii@gmail.com>
2020-02-09 14:05:15 +0100

    packaging: update license field

--- src/grapejuice_packaging/metadata.py
+++ src/grapejuice_packaging/metadata.py
@@ -3,7 +3,7 @@ from grapejuice import __version__ as package_version
 project_name = "Grapejuice"
 package_name = "grapejuice"
 package_version = package_version
-package_license = "GPL-3.0"
+package_license = "GPL-3"
 package_platform = "Linux"
 package_description = "A simple wine+roblox management application"
 package_repository = "https://gitlab.com/brinkervii/grapejuice"
