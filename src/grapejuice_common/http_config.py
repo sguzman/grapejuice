fdfe58c3a2be26a2bce9821ec7d0d268914b03a9
BrinkerVII <brinkervii@gmail.com>
2020-04-05 18:05:52 +0200

    http_config: Add logging

--- src/grapejuice_common/http_config.py
+++ src/grapejuice_common/http_config.py
@@ -1,7 +1,11 @@
+import logging
+
 import certifi
 import urllib3 as u3
 
 HTTP = None
+
+LOG = logging.getLogger(__name__)
 
 
 def http():
@@ -12,5 +16,7 @@ def http():
             cert_reqs="CERT_REQUIRED",
             ca_certs=certifi.where()
         )
+
+        LOG.debug("Configured HTTP pool manager")
 
     return HTTP
