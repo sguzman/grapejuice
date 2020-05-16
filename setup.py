5c3d2b41edf52554a6515f024e9c92be1efda697
BrinkerVII <brinkervii@gmail.com>
2020-05-16 19:17:42 +0200

    setup: Properly ignore packaging

--- setup.py
+++ setup.py
@@ -34,7 +34,12 @@ setup(
         'Programming Language :: Python :: 3.7'
     ],
     keywords=["grapejuice wine roblox studio"],
-    packages=find_packages("src", exclude=("grapejuice_packaging", "tests", "tests.*")),
+    packages=find_packages("src", exclude=[
+        "grapejuice_packaging",
+        "grapejuice_packaging.*",
+        "tests",
+        "tests.*"
+    ]),
     package_dir={"": "src"},
     include_package_data=True,
     python_requires=">=3.7",
